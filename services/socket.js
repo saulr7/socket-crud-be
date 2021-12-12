60a1bcf78138260d74817805da1798cc3049308f
Saul Ramirez <fernando.ramire@hotmail.com>
2021-12-12 13:59:45 -0600

    :sparkles: adding socket events

--- services/socket.js
+++ services/socket.js
@@ -14,6 +14,23 @@ class Sockets {
 
       // emit all bands
       socket.emit('current-bands', this.bandList.getBands());
+
+      socket.on('vote-band', ({ id }) => {
+        this.bandList.increaseVote(id);
+        this.io.emit('current-bands', this.bandList.getBands());
+      });
+      socket.on('delete-band', ({ id }) => {
+        this.bandList.deleteBand(id);
+        this.io.emit('current-bands', this.bandList.getBands());
+      });
+      socket.on('change-name', ({ id, name }) => {
+        this.bandList.changeBandName(id, name);
+        this.io.emit('current-bands', this.bandList.getBands());
+      });
+      socket.on('add-band', ({ name }) => {
+        this.bandList.addBand(name);
+        this.io.emit('current-bands', this.bandList.getBands());
+      });
     });
   }
 }
